a949189d0c352418265341ad6d23439395feb6a2
Narumol Phoowong <narumolphoowong@iMacvonNarumol.fritz.box>
2023-11-27 23:14:52 +0100

    added content

---
+++ json.js
@@ -0,0 +1,92 @@
+let questions = [
+    {
+        "image": "img/J.R.R. Tolkien.png",
+        "question": "Wer ist der Autor von Herr der Ringe?",
+        "answer_1": "A) J.K. Rowling",
+        "answer_2": "B) J.R.R. Tolkien",
+        "answer_3": "C) George R.R. Martin",
+        "answer_4": "D) C.S. Lewis",
+        "right_answer": 2
+    },
+    {
+        "image": "img/Minas Tirith.png",
+        "question": "Wie heißt die Hauptstadt von Gondor?",
+        "answer_1": "A) Minas Tirith",
+        "answer_2": "B) Rivendell",
+        "answer_3": "C) Helm's Deep",
+        "answer_4": "D) Lothlórien",
+        "right_answer": 1
+    },
+    {
+        "image": "img/elijah.jpeg",
+        "question": "Welcher Schauspieler spielt die Rolle von Frodo Beutlin im Film Herr der Ringe?",
+        "answer_1": "A) Elijah Wood",
+        "answer_2": "B) Daniel Radcliffe",
+        "answer_3": "C) Orlando Bloom",
+        "answer_4": "D) Sean Astin",
+        "right_answer": 1
+    },
+    {
+        "image": "img/Gollum.jpeg",
+        "question": "Welche Kreatur führt Frodo und Sam nach Mordor?",
+        "answer_1": "A) Gollum",
+        "answer_2": "B) Nazgûl",
+        "answer_3": "C) Balrog",
+        "answer_4": "D) Saruman",
+        "right_answer": 1
+    },
+    {
+        "image": "img/Gandalf.png",
+        "question": "Wer ist der Anführer der Ringgemeinschaft?",
+        "answer_1": "A) Frodo Beutlin",
+        "answer_2": "B) Aragorn",
+        "answer_3": "C) Gandalf",
+        "answer_4": "D) Legolas",
+        "right_answer": 3
+    },
+    {
+        "image": "img/smaug.png",
+        "question": "Wie heißt der Drache, den Bilbo im Hobbit besiegt?",
+        "answer_1": "A) Smaug",
+        "answer_2": "B) Sauron",
+        "answer_3": "C) Drogon",
+        "answer_4": "D) Falkor",
+        "right_answer": 1
+    },
+    {
+        "image": "img/Bruchtal.png",
+        "question": "In welcher Stadt beginnt die Reise der Ringgemeinschaft?",
+        "answer_1": "A) Rivendell",
+        "answer_2": "B) Beutelsend",
+        "answer_3": "C) Minas Tirith",
+        "answer_4": "D) Hobbingen",
+        "right_answer": 1
+    },
+    {
+        "image": "img/Legolas.png",
+        "question": "Welche Rasse gehört Legolas an?",
+        "answer_1": "A) Mensch",
+        "answer_2": "B) Zwerg",
+        "answer_3": "C) Hobbit",
+        "answer_4": "D) Elf",
+        "right_answer": 4
+    },
+    {
+        "image": "img/théoden.png",
+        "question": "Wer ist der König von Rohan?",
+        "answer_1": "A) Aragorn",
+        "answer_2": "B) Théoden",
+        "answer_3": "C) Denethor",
+        "answer_4": "D) Boromir",
+        "right_answer": 2
+    },
+    {
+        "image": "img/Arwen.png",
+        "question": "Wie nennt man die Elbenprinzessin, die Aragorn liebt?",
+        "answer_1": "A) Arwen",
+        "answer_2": "B) Galadriel",
+        "answer_3": "C) Éowyn",
+        "answer_4": "D) Tauriel",
+        "right_answer": 1
+    }
+];
